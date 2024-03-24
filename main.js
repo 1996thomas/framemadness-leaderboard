fb93113650efe1b9c557b9c6c5dffbcc0bdc70af
thomas1996 <thomas.kantahck@gmail.com>
2024-03-25 00:54:12 +0100

    adding input username and history choices

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import "./style.css";
+import { teamsData } from "./public/teams.js"; // Assurez-vous que le chemin est correct
 import usersChoices from "./utils/donnees_fusionnees.json";
 import matchPlayed from "./utils/tournament_progress.json";
+
 document.querySelector("#app").innerHTML = `
 <main>
 <div class="leaderBoard__wrapper">
@@ -9,7 +11,11 @@ document.querySelector("#app").innerHTML = `
     <img src="./Eco.png" height="40px" alt="" />
     <img src="./Krause_House.png" height="40px" alt="" />
   </div>
+  <div id="matchWinners">
+    <!-- Les vainqueurs des matchs seront affichés ici -->
+  </div>
   <h2>Leaderboard</h2>
+  <input type="text" id="searchInput" placeholder="Search by user...">
   <div class="leaderboard">
     <p>Leaderboard will start on March 21st</p>
   </div>
@@ -25,38 +31,38 @@ document.querySelector("#app").innerHTML = `
     proof forever as an NFT minted on Base.
   </div>
   <div class="schedule__wrapper">
-    <h2>Upcoming schedule</h2>
-    <ul>
-      <li>
-        <p class="date">17</p>
-        <div>
-          <p>March 2024</p>
-          <p>6pm ET - Submission start</p>
-        </div>
-      </li>
-      <li>
-        <p class="date">21</p>
-        <div>
-          <p>March 2024</p>
-          <p>12pm ET - Submission close</p>
-        </div>
-      </li>
-      <li>
-        <p class="date">08</p>
-        <div>
-          <p>April 2024</p>
-          <p>9pm ET - Tourney ends</p>
-        </div>
-      </li>
-      <li>
-        <p class="date">09</p>
-        <div>
-          <p>April 2024</p>
-          <p>10pm ET - NFTs are dropped with the prizes</p>
-        </div>
-      </li>
-    </ul>
-  </div>
+  <h2>Upcoming schedule</h2>
+  <ul>
+    <li>
+      <p class="date">17</p>
+      <div>
+        <p>March 2024</p>
+        <p>6pm ET - Submission start</p>
+      </div>
+    </li>
+    <li>
+      <p class="date">21</p>
+      <div>
+        <p>March 2024</p>
+        <p>12pm ET - Submission close</p>
+      </div>
+    </li>
+    <li>
+      <p class="date">08</p>
+      <div>
+        <p>April 2024</p>
+        <p>9pm ET - Tourney ends</p>
+      </div>
+    </li>
+    <li>
+      <p class="date">09</p>
+      <div>
+        <p>April 2024</p>
+        <p>10pm ET - NFTs are dropped with the prizes</p>
+      </div>
+    </li>
+  </ul>
+</div>
 </div>
 </main>
 `;
@@ -69,27 +75,15 @@ function updateLeaderboard(choixUtilisateurs, matchsJoues) {
     const avatar = utilisateur.userInformation.avatar;
 
     if (!scores[username]) {
-      scores[username] = { score: 0, username, avatar };
+      scores[username] = { score: 0, username, avatar, ucs: utilisateur.ucs };
     }
+
     utilisateur.ucs.forEach((choix) => {
       const matchJoue = matchsJoues.find(
         (matchJoue) => matchJoue.m === choix.m
       );
       if (matchJoue && choix.w === matchJoue.w) {
-        const matchId = choix.m;
-        if (matchId < 32) {
-          scores[username].score += 10;
-        } else if (matchId < 48) {
-          scores[username].score += 20;
-        } else if (matchId < 56) {
-          scores[username].score += 40;
-        } else if (matchId < 60) {
-          scores[username].score += 80;
-        } else if (matchId < 62) {
-          scores[username].score += 160;
-        } else if (matchId === 62) {
-          scores[username].score += 320;
-        }
+        scores[username].score += calculateScore(choix.m);
       }
     });
   });
@@ -102,31 +96,133 @@ function updateLeaderboard(choixUtilisateurs, matchsJoues) {
       <thead>
         <tr>
           <th></th>
-          <th></th>
-          <th></th>
+          <th>Username</th>
+          <th>Score</th>
         </tr>
       </thead>
       <tbody>
         ${leaderboard
           .map(
-            (user) => `
-              <tr class="userRow">
-                <td class="avatar">
-                  <a href="https://warpcast.com/${user.username}">
-                    <img src="${user.avatar}" alt="Avatar"/>
-                  </a>
-                </td>
-                <td class="username">${user.username}</td>
-                <td class="score">${user.score}</td>
-              </tr>
-          `
+            (user, index) => `
+            <tr class="userRow" id="userRow-${index}" style="cursor: pointer;">
+              <td class="avatar"><img src="${user.avatar}" alt="Avatar"></td>
+              <td class="username">${user.username}</td>
+              <td class="score">${user.score}</td>
+            </tr>
+            <tr id="dropdown-${index}" class="dropdownContent" style="display: none;">
+              <td colspan="3">
+                <select id="roundSelect-${index}" class="roundSelect">
+                <option value="">Choose a round</option>
+                <option value="1">Round 1</option>
+                <option value="2">Round 2</option>
+                <option value="3">Sweet 16</option>
+                <option value="4">Elite 8</option>
+                <option value="5">Final Four</option>
+                <option value="6">Finale</option>
+                <option value="all">See All</option>
+                </select>
+                <div id="userChoices-${index}"></div>
+              </td>
+            </tr>
+        `
           )
           .join("")}
       </tbody>
     </table>
   `;
+
+  attachEventListeners(leaderboard, matchsJoues);
+  attachRoundSelectListeners(leaderboard, matchsJoues);
+  attachSearchInputListener(leaderboard);
 }
 
-const choixUtilisateurs = usersChoices;
-const matchsJoues = matchPlayed;
-updateLeaderboard(choixUtilisateurs, matchsJoues);
+function calculateScore(matchId) {
+  if (matchId >= 63) return 320; // Final
+  else if (matchId >= 61) return 160; // Final Four
+  else if (matchId >= 57) return 80; // Elite 8
+  else if (matchId >= 49) return 40; // Sweet 16
+  else if (matchId >= 33) return 20; // Round 2
+  else return 10; // Round 1
+}
+
+function attachEventListeners(leaderboard, matchsJoues) {
+  document.querySelectorAll(".userRow").forEach((row, index) => {
+    row.addEventListener("click", () => {
+      const dropdown = document.getElementById(`dropdown-${index}`);
+      dropdown.style.display =
+        dropdown.style.display === "none" ? "table-row" : "none";
+    });
+  });
+}
+
+function attachRoundSelectListeners(leaderboard, matchsJoues) {
+  document.querySelectorAll(".roundSelect").forEach((selectElement, index) => {
+    selectElement.addEventListener("change", () => {
+      const round = selectElement.value;
+      updateChoicesDisplay(index, round, leaderboard, matchsJoues);
+    });
+  });
+}
+
+function attachSearchInputListener(leaderboard) {
+  const searchInput = document.getElementById("searchInput");
+  searchInput.addEventListener("input", () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    const usernames = leaderboard.map((user) => user.username.toLowerCase());
+    document.querySelectorAll(".userRow").forEach((row, index) => {
+      if (!usernames[index].includes(searchTerm)) {
+        row.style.display = "none";
+        const dropdown = document.getElementById(`dropdown-${index}`);
+        dropdown.style.display = "none";
+      } else {
+        row.style.display = "table-row";
+      }
+    });
+  });
+}
+
+function updateChoicesDisplay(
+  userIndex,
+  selectedRound,
+  leaderboard,
+  matchsJoues
+) {
+  const userChoicesElement = document.getElementById(
+    `userChoices-${userIndex}`
+  );
+  const user = leaderboard[userIndex];
+
+  const filteredChoices = user.ucs.filter((choice) => {
+    const matchRound = determineRound(choice.m);
+    return selectedRound === "all" || matchRound === parseInt(selectedRound);
+  });
+
+  userChoicesElement.innerHTML = filteredChoices
+    .map((u) => {
+      const matchResult = matchsJoues.find((m) => m.m === u.m);
+      const team = teamsData[u.w] || {};
+      const teamLogo = team.logo || "";
+      const teamName = team.name || "Team not found";
+      const borderColor = matchResult
+        ? matchResult.w === u.w
+          ? "#98FB98"
+          : "#B22222"
+        : "lightGray";
+      return `<div style="border: 2px solid ${borderColor}; border-radius: 50%; display: inline-block; padding: 5px; margin: 5px;">
+              <img src="${teamLogo}" height="40" width="40" alt="${teamName} logo"> 
+           </div>`;
+    })
+    .join("");
+}
+
+function determineRound(matchId) {
+  if (matchId >= 63) return 6;
+  else if (matchId >= 61) return 5;
+  else if (matchId >= 57) return 4;
+  else if (matchId >= 49) return 3;
+  else if (matchId >= 33) return 2;
+  else return 1;
+}
+
+// Exécuter la mise à jour du leaderboard après avoir défini toutes les fonctions nécessaires
+updateLeaderboard(usersChoices, matchPlayed);
---
+++ public/teams.js
@@ -0,0 +1,259 @@
+export  const teamsData = {
+  1: {
+    logo: "/east/01_uconn.png",
+    name: "Connecticut Huskies",
+  },
+  2: {
+    logo: "/east/02_stetson.png",
+    name: "Stetson Hatters",
+  },
+  3: {
+    logo: "/east/03_fla-atlantic.png",
+    name: "Florida Atlantic Owls",
+  },
+  4: {
+    logo: "/east/04_northwestern.png",
+    name: "Northwestern Wildcats",
+  },
+  5: {
+    logo: "/east/05_san-diego-st.png",
+    name: "San Diego State Aztecs",
+  },
+  6: {
+    logo: "/east/06_uab.png",
+    name: "UAB Blazers",
+  },
+  7: {
+    logo: "/east/07_auburn.png",
+    name: "Auburn Tigers",
+  },
+  8: {
+    logo: "/east/08_yale.png",
+    name: "Yale Bulldogs",
+  },
+  9: {
+    logo: "/east/09_byu.png",
+    name: "Brigham Young Cougars",
+  },
+  10: {
+    logo: "/east/10_duquesne.png",
+    name: "Duquesne Dukes",
+  },
+  11: {
+    logo: "/east/11_illinois.png",
+    name: "Illinois Fighting Illini",
+  },
+
+  12: {
+    logo: "/east/12_morehead-st.png",
+    name: "Morehead State Eagles",
+  },
+  13: {
+    logo: "/east/13_washington-st.png",
+    name: "Washington State Cougars",
+  },
+  14: {
+    logo: "/east/14_drake.png",
+    name: "Drake Bulldogs",
+  },
+  15: {
+    logo: "/east/15_iowa-st.png",
+    name: "Iowa State Cyclones",
+  },
+  16: {
+    logo: "/east/16_south-dakota-st.png",
+    name: "South Dakota State Jackrabbits",
+  },
+  17: {
+    logo: "/west/01_north-carolina.png",
+    name: "North Carolina Tar Heels",
+  },
+  18: {
+    logo: "/west/02_west_howard_wagner.png",
+    name: " Howard/Wagner",
+  },
+  19: {
+    logo: "/west/03_mississippi-st.png",
+    name: "Mississippi State Bulldogs",
+  },
+  20: {
+    logo: "/west/04_michigan-st.png",
+    name: "Michigan State Spartans",
+  },
+  21: {
+    logo: "/west/05_st-marys-ca.png",
+    name: "Saint Mary's Gaels",
+  },
+  22: {
+    logo: "/west/06_grand-canyon.png",
+    name: "Grand Canyon Antelopes",
+  },
+  23: {
+    logo: "/west/07_alabama.png",
+    name: "Alabama Crimson Tide",
+  },
+  24: {
+    logo: "/west/08_col-of-charleston.png",
+    name: "Charleston Cougars",
+  },
+  25: {
+    logo: "/west/09_clemson.png",
+    name: "Clemson Tigers",
+  },
+  26: {
+    logo: "/west/10_new-mexico.png",
+    name: "New Mexico Lobos",
+  },
+  27: {
+    logo: "/west/11_baylor.png",
+    name: "Baylor Bears",
+  },
+  28: {
+    logo: "/west/12_colgate.png",
+    name: "Colgate Raiders",
+  },
+  29: {
+    logo: "/west/13_dayton.png",
+    name: "Dayton Flyers",
+  },
+  30: {
+    logo: "/west/14_nevada.png",
+    name: "Nevada Wolf Pack",
+  },
+  31: {
+    logo: "/west/15_arizona.png",
+    name: "Arizona Wildcats",
+  },
+  32: {
+    logo: "/west/16_long-beach-st.png",
+    name: "Long Beach State 49ers",
+  },
+  33: {
+    logo: "/south/01_houston.png",
+    name: "Houston Cougars",
+  },
+  34: {
+    logo: "/south/02_longwood.png",
+    name: "Longwood Lancers",
+  },
+  35: {
+    logo: "/south/03_nebraska.png",
+    name: "Nebraska Cornhuskers",
+  },
+  36: {
+    logo: "/south/04_texas-am.png",
+    name: "Texas A&M Aggies",
+  },
+  37: {
+    logo: "/south/05_wisconsin.png",
+    name: "Wisconsin Badgers",
+  },
+  38: {
+    logo: "/south/06_james-madison.png",
+    name: "James Madison Dukes",
+  },
+  39: {
+    logo: "/south/07_duke.png",
+    name: "Duke Blue Devils",
+  },
+  40: {
+    logo: "/south/08_vermont.png",
+    name: "Vermont Catamounts",
+  },
+  41: {
+    logo: "/south/09_texas-tech.png",
+    name: "Texas Tech Red Raiders",
+  },
+  42: {
+    logo: "/south/10_north-carolina-st.png",
+    name: "North Carolina State Wolfpack",
+  },
+  43: {
+    logo: "/south/11_kentucky.png",
+    name: "Kentucky Wildcats",
+  },
+  44: {
+    logo: "/south/12_oakland.png",
+    name: "Oakland Golden Grizzlies",
+  },
+  45: {
+    logo: "/south/13_florida.png",
+    name: "Florida Gators",
+  },
+  46: {
+    logo: "/south/14_south_colorado_boise-st.png",
+    name: "Colorado/Boise St",
+  },
+  47: {
+    logo: "/south/15_marquette.png",
+    name: "Marquette Golden Eagles",
+  },
+  48: {
+    logo: "/south/16_western-ky.png",
+    name: "Western Kentucky Hilltoppers",
+  },
+  49: {
+    logo: "/midwest/01_purdue.png",
+    name: "Purdue Boilermakers",
+  },
+  50: {
+    logo: "/midwest/02_midewest_grambling_montana-st.png",
+    name: "Grambling/Montana St",
+  },
+  51: {
+    logo: "/midwest/03_utah-st.png",
+    name: "Utah State Aggies",
+  },
+  52: {
+    logo: "/midwest/04_tcu.png",
+    name: "TCU Horned Frogs",
+  },
+  53: {
+    logo: "/midwest/05_gonzaga.png",
+    name: "Gonzaga Bulldogs",
+  },
+  54: {
+    logo: "/midwest/06_mcneese.png",
+    name: "Mcneese State Cowboys",
+  },
+  55: {
+    logo: "/midwest/07_kansas.png",
+    name: "Kansas Jayhawks",
+  },
+  56: {
+    logo: "/midwest/08_samford.png",
+    name: "Samford Bulldogs",
+  },
+  57: {
+    logo: "/midwest/09_south-carolina.png",
+    name: "South Carolina Gamecocks",
+  },
+  58: {
+    logo: "/midwest/10_oregon.png",
+    name: "Oregon Ducks",
+  },
+  59: {
+    logo: "/midwest/11_creighton.png",
+    name: "Creighton Bluejays",
+  },
+  60: {
+    logo: "/midwest/12_akron.png",
+    name: "Akron Zips",
+  },
+  61: {
+    logo: "/midwest/13_texas.png",
+    name: "Texas Longhorns",
+  },
+  62: {
+    logo: "/midwest/14_midwest_colorado-st_virginia.png",
+    name: "Colorodo St/Virginia",
+  },
+  63: {
+    logo: "/midwest/15_tennessee.png",
+    name: "Tennessee Volunteers",
+  },
+  64: {
+    logo: "/midwest/16_st-peters.png",
+    name: "Saint Peter's Peacocks",
+  },
+};
